4fa22d8ffe848cdbe9b8f44711d61990f30e88af
yukisato <yuki01377@gmail.com>
2022-01-19 09:19:27 +0900

    user model add

--- railstutorialv2/Program.cs
+++ railstutorialv2/Program.cs
@@ -1,7 +1,35 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+using Dapper;
+using railstutorialv2.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddScoped(_ => new MySqlConnection(connectionString));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if(!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error");
+}
 
+app.MapGet("/error", () => Results.Problem("An error occurred.", statusCode: 500))
+    .ExcludeFromDescription();
+
+app.MapSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", () => "Hello World!")
+    .WithName("Hello");
+
+app.MapGet("/hello", () => new { Hello = "World" })
+    .WithName("HelloObject");
+
+app.MapGet("/users", async (MySqlConnection db) =>
+    await db.QueryAsync<User>("select * from users"))
+    .WithName("GetAllUsers");
 app.Run();
 
